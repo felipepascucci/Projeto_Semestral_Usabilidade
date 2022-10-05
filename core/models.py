8431160f9c18057fe4775292e397db66ae7887c3
Renan Pires <renan.sp.121@hotmail.com>
2022-10-05 01:20:31 -0300

    Renan: criando banco de dados de cliente

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=125)
+    dt_nasc = models.DateField
+    email = models.CharField(max_length=125)
+    cidade = models.CharField(max_length=100)
+    pais = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11)
+    
+    
